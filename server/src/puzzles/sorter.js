46356211078a6264a222dfcf1205483e2ad31a19
Michael Paulson <mpaulson@netflix.com>
2020-10-23 14:04:27 -0700

    feat(new-game): yayayaya

---
+++ server/src/puzzles/sorter.js
@@ -0,0 +1,32 @@
+module.exports.start = function() {
+    const sort_me = [
+        "c",
+        "3",
+        "p",
+        "o",
+    ];
+
+    const sort_me = [
+        4,
+        5,
+        69,
+        420,
+    ];
+};
+
+module.exports.end = function() {
+    const sort_me = [
+        "3",
+        "c",
+        "o",
+        "p",
+    ];
+
+    const sort_me = [
+        4,
+        420,
+        5,
+        69,
+    ];
+};
+
